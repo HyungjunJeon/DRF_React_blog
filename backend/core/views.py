73ad071890569bb68a8cef607e0abf87a6047ca3
Hyungjun <maru105kr@gmail.com>
2022-12-18 22:27:10 +0900

    Serializers & View

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework import viewsets
+from .serializers import ArticleSerializer, UserSerializer
+from .models import Article
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    authentication_classes = (TokenAuthentication)
