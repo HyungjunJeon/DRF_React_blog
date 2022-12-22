38464d472631baad701418917116a64a5dd23fea
Hyungjun <maru105kr@gmail.com>
2022-12-22 18:17:34 +0900

    Create Main Page

---
+++ frontend/src/component/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ArticleList = () => {
+  return (
+    <div>
+      <h1>List of Articles</h1>
+      <p>Hi there</p>
+    </div>
+  )
+}
+
+export default ArticleList
